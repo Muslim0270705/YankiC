40a70311767f6fd0ef676c51901d234e5af424ac
Chinara06 <toktogulovacinara8@gmail.com>
2024-03-07 19:05:59 +0600

    register

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
 const Register = () => {
     return (
-        <p>
-            Register
-        </p>
-    );
-};
+        <div className="register">
+        <form  className= "register__form">
+                <h2 className="register__title">
+                    Регистратция
+                </h2>
+            <div className="register__row">
+                <input  placeholder={"Ваше имя*"} className={"register__inout"} type="text"/>
+                <input  name={"email"} placeholder={"Ваш e-mail*"} className={"register__inout"} type="email"/>
+                <input  placeholder={"Ваш пароль*"} className={"register__inout"} type="password"/>
+                <button className="register__btn" type={"submit"}>
+                    ПРОДОЛЖИТЬ
+                </button>
+            </div>
+        </form>
+</div>
+
+    )}
+
+
 
 export default Register;
