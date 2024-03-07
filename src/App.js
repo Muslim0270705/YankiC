b11d121c9cce04f85f361a2cfa305bec5798e7c2
Muslim Mageza <muslimmageza@MacBook-Pro-Muslim.local>
2024-03-07 18:27:42 +0600

    fix bag

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import Home from "./pages/Home/Home";
 import Catalog from "./pages/Catalog/Catalog";
 import "./styles/style.scss"
 import Register from "./pages/Auth/Register/Register";
-<<<<<<< HEAD
 import Basket from "./pages/Basket/Basket";
-=======
 import Product from "./pages/Product/Product";
->>>>>>> 71269d6 (add oneProduct)
 function App() {
   return (
     <Routes>
       <Route path={"/"} element={<Layout/>}>
         <Route path={"/"} element={<Home/>}/>
         <Route path={"/catalog"} element={<Catalog/>}/>
-<<<<<<< HEAD
         <Route path={"/basket"} element={<Basket/>}/>
-=======
         <Route path={"/product/:id"} element={<Product/>}/>
->>>>>>> 71269d6 (add oneProduct)
       </Route>
         <Route path={"/register"} element={<Register/>}/>
     </Routes>
